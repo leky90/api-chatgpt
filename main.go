d3c92f399d0813170ce24de86eee6aaa3430ed41
Ky Le <ldky90@gmail.com>
2023-08-01 18:10:02 +0700

    update netlify deploy

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 	}
 
 	s := http.Server{
-		Addr:    ":80",
+		Addr:    ":8080",
 		Handler: h2c.NewHandler(e, h2s),
 	}
 	if err := s.ListenAndServe(); err != http.ErrServerClosed {
